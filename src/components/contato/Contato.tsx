7285d5fb8f8e719074d07772c026b547e14eec83
Vinicius Silva Souza <viniciusvss120@gmail.com>
2023-08-13 00:30:47 -0400

    Fazendo a sessão de contato

--- src/components/contato/Contato.tsx
+++ src/components/contato/Contato.tsx
@@ -2,8 +2,25 @@ import style from './Contato.module.css'
 
 export default function Contato(){
   return (
-    <div>
+    <section className={style.container}>
       <h1>Contato</h1>
-    </div>
+      <div className={style.contato}>
+        <p>Pode entrar em contato enviando um e-mail, através 
+          desse formulário ou pelas minhas redes socias logo abaixo.
+        </p>
+        <form className={style.formulario}>
+          <input type="text" placeholder='Digite seu nome'/>
+          <input type="email" placeholder='Digite seu e-mail' />
+          <textarea name="mensagem" placeholder='Menssagem' cols={117} rows={10}></textarea>
+          <button type="submit">Enviar</button>
+        </form>
+        <div className={style.divisa}>
+          <span></span>
+          <p>ou</p>
+          <span></span>
+        </div>
+        <div className={style.icons}></div>
+      </div>
+    </section>
   )
 }
